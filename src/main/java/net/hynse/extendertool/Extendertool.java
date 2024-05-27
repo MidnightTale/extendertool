47b52706aace8f8b655b19748d83b707c3ad2244
MidnightTale <midnighttale24@gmail.com>
2024-05-27 11:18:31 +0700

    unbreaking enchantment logic

--- src/main/java/net/hynse/extendertool/Extendertool.java
+++ src/main/java/net/hynse/extendertool/Extendertool.java
@@ -7,6 +7,8 @@ import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.enchantments.EnchantmentTarget;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -19,6 +21,8 @@ import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Map;
+import java.util.Random;
 import java.util.UUID;
 
 public final class Extendertool extends JavaPlugin implements Listener {
@@ -94,9 +98,27 @@ public final class Extendertool extends JavaPlugin implements Listener {
             NamespacedKey toolKey = new NamespacedKey(this, CUSTOM_TOOL_KEY);
 
             if (container.has(toolKey, PersistentDataType.BYTE)) {
-                short currentDurability = item.getDurability();
-                item.setDurability((short) (currentDurability - 1));
+                if (shouldLoseDurability(item)) {
+                    short currentDurability = item.getDurability();
+                    item.setDurability((short) (currentDurability + 1));
+                }
             }
         }
     }
 
+    private boolean shouldLoseDurability(ItemStack item) {
+        if (item.containsEnchantment(Enchantment.UNBREAKING)) {
+            int unbreakingLevel = item.getEnchantmentLevel(Enchantment.UNBREAKING);
+            Random random = new Random();
+            return random.nextInt(100) >= calculateUnbreakingChance(unbreakingLevel);
+        }
+        return true;
+    }
+
+    private int calculateUnbreakingChance(int unbreakingLevel) {
+        if (unbreakingLevel > 0) {
+            return 100 / (unbreakingLevel + 1);
+        }
+        return 100;
+    }
+}
